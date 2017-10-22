cf587ef75bcf1a4357e7d03c95c943742c46c08e
Christian Zufferey <christian@zufferey.com>
2017-10-22 21:48:53 +0100

    - ajouté des sleep 1 un peu partout, je dois avoir des problèmes de vitesse avec certaines SD

--- dietpi-clone.sh
+++ dietpi-clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # petit script pour cloner une carte SD DietPi sur une autre, mais plus petite
-#zf171022.2113
+#zf171022.2248
 
 #source: https://github.com/billw2/rpi-clone
 
@@ -8,40 +8,45 @@ zSD="/dev/sda"
 zSD1=$zSD"1"
 zSD2=$zSD"2"
 
-:<<'zdebug'
+#:<<'zdebug'
 
 
 #démonte la nouvelle SD
 umount $zSD1
 umount $zSD2
+sleep 1
 
 #efface toute les partition de la nouvelle SD
 (echo d; echo ; echo d; echo ; echo d; echo ; echo w) | fdisk $zSD
+sleep 1
 
 #crée les partions de la nouvelle SD et les formates
 (echo n; echo p; echo 1; echo ; echo +100M; echo w) | fdisk $zSD
 (echo n; echo p; echo 2; echo ; echo +3.7G; echo w) | fdisk $zSD
+sleep 1
 mkfs -t vfat -F 32 $zSD1
 (echo y) | mkfs -t ext4 $zSD2
+sleep 1
 
 #copie la partion 2 sur la nouvelle SD
 mkdir /mnt/clone
 mount $zSD2 /mnt/clone
 rsync --progress --force -rltWDEHXAgoptx --delete --exclude /var/swap --exclude .gvfs --exclude '/dev/*' --exclude '/mnt/clone/*' --exclude '/proc/*' --exclude '/run/*' --exclude '/sys/*' --exclude '/tmp/*' --exclude 'lost\+found/*' // /mnt/clone
+sleep 1
 
 #copie la partion 21 sur la nouvelle SD
 mkdir /mnt/clone/boot
 mount $zSD1 /mnt/clone/boot
 rsync --progress --force -rltWDEHXAgoptx --delete --exclude .gvfs --exclude 'lost\+found/*' /boot/ /mnt/clone/boot
-
-
-exit
-
-zdebug
+sleep 1
 
 
 
-zUUID=`blkid |grep /dev/sda2 |awk '{print $2}'`
+#zdebug
+
+
+
+zUUID=`blkid |grep $zSD2 | awk '{print $2}' | sed 's/\"//g'`
 echo $zUUID
 
 cat /mnt/clone/boot/armbianEnv.txt |grep rootdev=
@@ -53,6 +58,7 @@ cat /mnt/clone/boot/armbianEnv.txt |grep rootdev=
 sync
 sync
 sync
+sleep 1
 umount $zSD1
 umount $zSD2
 
